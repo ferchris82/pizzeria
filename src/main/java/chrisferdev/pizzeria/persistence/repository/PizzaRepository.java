2d959a17f6bfe5d9c8863d22222f90e3d9e7e3e3
ChrisferDev <ferchris82@hotmail.com>
2024-05-01 12:02:25 -0500

    Versión 1.2

--- src/main/java/chrisferdev/pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/chrisferdev/pizzeria/persistence/repository/PizzaRepository.java
@@ -1,9 +1,12 @@
 package chrisferdev.pizzeria.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 import chrisferdev.pizzeria.persistence.entity.PizzaEntity;
 
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer>{
-    
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+    PizzaEntity findAllByAvailableTrueAndNameIgnoraCase(String name);
 }
