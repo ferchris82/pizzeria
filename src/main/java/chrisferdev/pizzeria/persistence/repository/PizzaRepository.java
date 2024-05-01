e6b06c22a9a489fc1c8cf712e88652fbb702caad
ChrisferDev <ferchris82@hotmail.com>
2024-05-01 13:40:29 -0500

    Versión 1.2

--- src/main/java/chrisferdev/pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/chrisferdev/pizzeria/persistence/repository/PizzaRepository.java
@@ -9,8 +9,5 @@ import chrisferdev.pizzeria.persistence.entity.PizzaEntity;
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer>{
     List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
     PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
-<<<<<<< HEAD
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
-=======
->>>>>>> 2faae9e6932e11e4f5ad7c50321d5465563538fc
 }
