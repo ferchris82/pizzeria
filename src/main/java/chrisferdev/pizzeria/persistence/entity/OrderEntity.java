6aca23311c5366a1b01f04cf4b70dd80be97aeab
ChrisferDev <ferchris82@hotmail.com>
2024-05-02 12:13:38 -0500

    Versión 1.3

--- src/main/java/chrisferdev/pizzeria/persistence/entity/OrderEntity.java
+++ src/main/java/chrisferdev/pizzeria/persistence/entity/OrderEntity.java
@@ -5,16 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -52,5 +43,6 @@ public class OrderEntity {
     private CustomerEntity customer;
 
     @OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
+    @OrderBy("price DESC")
     private List<OrderItemEntity> items;
 }
--- src/main/java/chrisferdev/pizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/chrisferdev/pizzeria/persistence/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package chrisferdev.pizzeria.persistence.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import chrisferdev.pizzeria.persistence.entity.OrderEntity;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,6 +13,9 @@ import java.util.List;
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer>{
     List<OrderEntity> findAllByDateAfter(LocalDateTime Date);
     List<OrderEntity> findAllByMethodIn(List<String> methods);
+
+    @Query(value = "SELECT * FROM pizza_order WHERE id_customer = :id", nativeQuery = true)
+    List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
 }
 
 
